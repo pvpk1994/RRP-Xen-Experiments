dfffa74af7290fce75bcf687235a355ca9052531
Pavan Kumar Paluri <pvpk1994@gmail.com>
2021-12-10 14:15:36 -0600

    run as ./redis_helper.sh <PID of redis-cli process>

--- Redis-Cli/redis_helper.sh
+++ Redis-Cli/redis_helper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Run as ./redis_helper.sh <PID>
 # This script serves as a helper script to run redis-cli as a real-time process with #1 priority
 # and to bind this process to either VCPU #0 or VCPU #1 of a given VM in a 2-VCPU per VM. 
 # In this example script, the redis process is hard-pinned to CPU #1 i.e., VCPU#1 
